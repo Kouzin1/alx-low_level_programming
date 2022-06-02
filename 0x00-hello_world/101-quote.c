268c42feaa9bb83832f86678d531706559df8610
kouzin1 <samuelakinwunmi2@gmail.com>
2022-06-02 14:16:24 -0700

    Write

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -2,6 +2,6 @@
 #include <unistd.h>
 int main(void)
 {
-	write("and that pieceof art is useful\" - Dore Korpar, 2015-10-19");
+	write(2, "and that pieceof art is useful\" - Dore Korpar, 2015-10-19\n" 59);
 	return (1);
 }
