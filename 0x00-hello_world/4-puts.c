fcaf2c796dbff36898d0368d3e0159563bc85292
kouzin1 <samuelakinwunmi2@gmail.com>
2022-06-02 15:58:15 -0700

    Put_update

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 /**
- * prints the phrase"programming is like building a multilingual puzzle
- * 0 if exited properly, non-zero otherwise
+ *main - prints the phrase"programming is like building a multilingual puzzle
+ * Return: 0 if exited properly, non-zero otherwise
  */
 
 int main(void)
