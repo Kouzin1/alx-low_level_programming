36cd9ff798467ca9393e72d028d7843a1003c6e1
kouzin1 <samuelakinwunmi2@gmail.com>
2022-06-07 18:59:39 -0700

    islower

--- 0x02-functions_nested_loops/104-fibonacci.c
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -1,14 +1,14 @@
 #include <stdio.h>
 
 /**
- * main - finds and prints the first 98 Fibonacci numbers,
+ * main - finds and prints the first 100 Fibonacci numbers,
  * starting with 1 and 2
  * followed by a new line
  * Return: Always 0 (Success)
  */
 int main(void)
 {
-	unsigned long int i,j, k, j1, j2, k1, k2;
+	unsigned long int i, j, k, j1, j2, k1, k2;
 
 	j = 1;
 	k = 2;
--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -3,7 +3,7 @@
 /**
  * _islower - checks for lowercase character
  * @c: the character to check
- * REturn: 1 if c is lowercase, 0 therwise
+ * Return: 1 if c is lowercase, 0 therwise
  */
 int _islower(int c)
 {
