eb68ef749439a23ad82cc6529eb7fc06a092da11
kouzin1 <samuelakinwunmi2@gmail.com>
2022-06-17 11:25:24 -0700

    Infinite

--- 0x06-pointers_arrays_strings/103-infinite_add.c
+++ 0x06-pointers_arrays_strings/103-infinite_add.c
@@ -10,23 +10,52 @@
  *
  * Return: If r can store the sum - a pointer to the result else return 0.
  */
+int _atoi(char *s)
+{
+	int sign = 1, resp = 0, firstNum;
 
+	for (firstNum = 0; !(s[firstNum] >= 48 && s[firstNum] <= 57); firstNum++)
+	{
+		if (s[firstNum] == '-')
+		{
+			sign *= -1;
+		}
+	}
+
+	for (int i = firstNum; s[i] >= 48 && s[i] <= 57; i++)
+	{
+		resp *= 10;
+		resp += (s[i] - 48);
+	}
+
+	return (sign * resp);
+}
+
+void int_to-string(int n)
+{
+	int divisor = 1, i, resp;
+
+	for (i = 0; n / divisor > 9; i++)
+	{
+		divisor *= 10;
+	}
+
+	char str[i];
+
+	for (int cmpt = 0; divisor >= 10; divisor /= 10, cmpt++)
+	{
+		resp = n / divisor;
+		str[cmpt] = '0' + resp;
+		n = n - resp * divisor;
+	}
+	str[i] = ('0' + n);
+
+}
 char *infinite_add(char *n1, char *n2, char *r, int r_index)
 {
-	int index, n1_len = 0, n2_len = 0;
+	int sum, a, b;
+	a = _atoi(n1);
+	b = _atoi(n2);
 
-	for (index = 0; *(n1 + index); index++)
-		n1_len++;
-
-	for (index = 0; *(n2 + index); index++)
-		n2_len++;
-
-	if (size_r <= n1_len + 1 || size_r <= n2_len + 1)
-		return (0);
-
-	n1 += n1_len - 1;
-	n2 += n2_len - 1;
-	*(r + size_r) = '\0';
-
-	return (add_strings(n1, n2, r, --size_r));
+	sum = a + b;
 }
