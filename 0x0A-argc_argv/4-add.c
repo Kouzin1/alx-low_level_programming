d1309b99ffb26e0f2d75da95c06a51ec0bf1ae1d
kouzin1 <samuelakinwunmi2@gmail.com>
2022-06-25 13:34:44 -0700

    Additions

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -2,83 +2,39 @@
 #include <stdio.h>
 #include <stdlib.h>
 /**
- * _atoi - converts a string to an integer
- * @s: string to be converted
- *
- * Return: the int converted from the string
- */
-int _atoi(char *s)
-{
-	int i, d, n, len, f, digit;
-
-	i = 0;
-	d = 0;
-	n = 0;
-	len = 0;
-	f = 0;
-	digit = 0;
-
-	while (s[len] != '\0')
-		len++;
-
-	while (i < len && f == 0)
-	{
-		if (s[i] == '-')
-			++d;
-
-		if (s[i] >= '0' && s[i] <= '9')
-		{
-			digit = s[i] - '0';
-			if (d % 2)
-				digit = -digit;
-			n = n * 10 + digit;
-			f = 1;
-			if (s[i + 1] < '0' || s[i + 1] > '9')
-				break;
-			f = 0;
-		}
-		i++
-	}
-
-	if (f == 0)
-		return (0);
-
-	return (n);
-}
-/**
  * main - adds two positive number
  * @argc: number of a guments
  * @argv: array of arguments
  *
  * Return: 0 (Success), or 1(Error)
  */
+
 int main(int argc, char *argv[])
 {
-	int sum, num, i, j, k;
+	int i, j, add = 0;
 
-	sum = 0;
-
-	for (i = 1; i < argc; i++)
+	if (argc > 1)
 	{
-		for (j = 0; argv[i][j] != '\0'; j++)
+
+		for (i = 1; i < argc; i++)
 		{
-			if (argv[i][j] > '9' || argv[i][j] < '0')
+			for (j = 0; argv[i][j] != '\0'; j++)
 			{
-				puts("Error");
-				return (1);
+				if (argv[i][j] < 48 || argv[i][j] > 57)
+				{
+					printf("Error\n");
+					return (1);
+				}
 			}
+			add += atoi(argv[i]);
 		}
+
+		printf("%d\n", add);
 	}
 
-	for (k = 1; k < argc; k++)
+	else if (argc == 1)
 	{
-		num = _atoi(argv[k]);
-		if (num >= 0)
-		{
-			sum += num;
-		}
+		print("0\n");
 	}
-
-	printf("%d\n", sum);
 	return (0);
 }
