f903e26ccb2c8ffef5a594a38b4f3943dfcdd380
kouzin1 <samuelakinwunmi2@gmail.com>
2022-07-06 13:56:58 -0700

    Calculation

--- 0x0F-function_pointers/3-main.c
+++ 0x0F-function_pointers/3-main.c
@@ -3,7 +3,7 @@
 /**
  * main - program that performs simple operations'
  * @argc: number of arguments
- * @argv array of arguments
+ * @argv: array of arguments
  *
  * Return: Always 0 (Succcess)
  */
