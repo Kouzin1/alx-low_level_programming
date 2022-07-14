7e129ea090a571992cb178eee4b42ef141ce7cc4
kouzin1 <samuelakinwunmi2@gmail.com>
2022-07-14 07:19:43 -0700

    Updated

---
+++ 0x12-singly_linked_lists/2-main.c
@@ -0,0 +1,21 @@
+#include <stdlib.h>
+#include <string.h>
+#include <stdio.h>
+#include "lists.h"
+
+/**
+ * main - check the code
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+	list_t *head;
+
+	head = NULL;
+	add_node(&head, "Samuel");
+	add_node(&head, "Please");
+	print_list(head);
+	return (0);
+}
+
